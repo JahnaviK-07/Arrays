5970fd2b1d5e671cc1dbf1e1ead593b3fc1f451f
Jahnavi Kaasu <1kasujah@hdsb.ca>
2025-06-16 20:48:45 -0400

    fixing the issues

--- myScript.js
+++ myScript.js
@@ -1,43 +1,24 @@
 const cars = ["Saab","Volvo","BMW","Mazda"];
 
+console.log("Printing the cars array using index \n");
+console.log(cars[0]);
+console.log(cars[1]);
+console.log(cars[2]);
+console.log(cars[3]);
+
 cars[0] = "Honda";
 
+console.log("Printing the cars at index 0 after modifhying the value \n");
+console.log(cars[0]);
 
-const fruits = ["Banana","Orange","Lemon","Apple","Mango"];
-const cirtus = fruits.slice(1,3);
-console.log("Slicing fruits array");
-console.log(cirtus);
+const gameStatus = ["You lost the game!", "You Won the game!"];
+let result=1;
+console.log("Printing the cars gameStatus array using index \n");
+console.log(gameStatus[result])
 
-const fruits_splice = ["Banana","Orange","Lemon","Apple","Mango"];
-fruits_splice.splice(2,0,"Lemon","Kiwi");
-console.log("Splicing fruits array");
-console.log(fruits_splice);
+console.log("Printing cars using for each");
+cars.forEach( (car, index) => {console.log(car);});
 
-fruits_pop = ["Banana","Orange","Lemon","Apple","Mango"];
-fruits_pop.pop();
-console.log("Using pop on the fruits array");
-console.log(fruits_pop);
-
-fruits_push = ["Banana","Orange","Lemon","Apple","Mango"];
-fruits_push.push("Kiwi");
-console.log("Using push on the fruits array");
-console.log(fruits_push);
-
-
-
-fruits_indexOf = ["Banana","Orange","Lemon","Apple","Mango"];
-let index = fruits_indexOf.indexOf("Apple");
-console.log("Using indexOf on the fruits array");
-console.log(index);
-
-
-const banana = Array.from("Banana");
-console.log("Using Arrays.from on the fruits array");
-console.log(banana);
-
-
-fruits_includes = ["Banana","Orange","Lemon","Apple","Mango"];
-result = fruits_includes.includes("Mango");
-console.log("Using includes on the fruits array");
-console.log(result);
+console.log("Printing cars using for each with index");
+cars.forEach( (car, index) => {console.log("Index:" + index + " Car Name: " + car);});
 
